217d11a240298fe6a59252e95af1fc51ab49b754
SimranSingh010 <simran.ahuja0010@gmail.com>
2026-01-29 13:15:31 +0530

    chore: Configure CORS to allow all origins.

--- backend/src/main/java/com/bloodreport/analyzer/controller/BloodReportController.java
+++ backend/src/main/java/com/bloodreport/analyzer/controller/BloodReportController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/blood-report")
-@CrossOrigin(origins = "http://localhost:5173") // React dev server
+@CrossOrigin(origins = "*") // Allow all origins for now - will update with specific Vercel URL
 public class BloodReportController {
 
     @Autowired
